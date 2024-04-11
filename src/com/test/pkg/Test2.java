80a6dbadde59bbfae6f41582441ffdb202dad2e3
highlandwr <highlandwr@gmail.com>
2024-04-11 16:19:31 +0530

    committing Test2 to BranchX

---
+++ src/com/test/pkg/Test2.java
@@ -0,0 +1,14 @@
+package com.test.pkg;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if("yes".equals(hungry)) {
+			System.out.println("eat something...");
+		}else {
+			System.out.println("Do work...");
+		}
+	}
+}
